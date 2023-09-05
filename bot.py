5188628e77e854ea567fa3211aadf2ff9c05d338
Zoldrine97 <139350525+Zoldrine97-V2@users.noreply.github.com>
2023-09-05 22:27:11 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,12 @@
+import discord
 
+def run_discord_bot():
+    TOKEN = 'MTE0ODcyMjk2MDE1NzM5NzAxNA.G13KW2.oiwvt_ZJUlEOQx1yRew40oqzTYGLARGdkJL_84'
+    client = discord.Client()
+    
+    @client.event
+    async def on_ready():
+        print(f'{client.user} is now running!')
+        
+
+    client.run(TOKEN)
